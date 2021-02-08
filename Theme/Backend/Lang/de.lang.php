9683fdaf32fab0af17c96e9e2eea0fe131a7ad10
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-02-08 19:10:15 +0100

    test language/export/import which changed a lot of lang files

---
+++ Theme/Backend/Lang/de.lang.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['Messages' => [
+    'All' => '',
+    'AverageAmount' => '',
+    'BCC' => '',
+    'CC' => '',
+    'Date' => '',
+    'Draft' => '',
+    'From' => '',
+    'Inbox' => '',
+    'Interval' => '',
+    'Mailboxes' => '',
+    'Messages' => '',
+    'Month' => '',
+    'New' => '',
+    'Outbox' => '',
+    'Received' => '',
+    'Sent' => '',
+    'Spam' => '',
+    'Statistics' => '',
+    'Status' => '',
+    'Subject' => '',
+    'Tag' => '',
+    'To' => '',
+    'Today' => '',
+    'Trash' => '',
+    'Week' => '',
+    'Year' => '',
+    'Select' => '',
+    'Save' => '',
+    'Delete' => '',
+]];
