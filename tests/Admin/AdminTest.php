57d2fa5947f727f3373cb80d5955df4983187cc9
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-10-21 22:16:06 +0200

    more tests and make classes final

---
+++ tests/Admin/AdminTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   tests
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+namespace Modules\Messages\tests\Admin;
+
+/**
+ * @internal
+ */
+class AdminTest extends \PHPUnit\Framework\TestCase
+{
+    protected const NAME = 'Messages';
+
+    protected const URI_LOAD = 'http://127.0.0.1/en/backend/message';
+
+    use \Modules\tests\ModuleTestTrait;
+}
